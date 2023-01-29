739f37e7cc85be998625de4aad5dcf968b88579b
Julia Sokolova <anika.streim@yandex.ru>
2023-01-29 07:04:53 +0300

    feat: добавить редактирование поп апа

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,38 @@
-const editButton = document.querySelector(".profile__edit");
-
-const popup = document.querySelector(".popup");
-const closeButton = popup.querySelector(".popup__close");
-
-const toggleOpenPopup = () => {
-  popup.classList.toggle("popup_open");
+let editButton = document.querySelector(".profile__edit");
+let popup = document.querySelector(".popup");
+let closeButton = popup.querySelector(".popup__close");
+function toggleOpenPopup() {
+  popup.classList.toggle("popup_opened");
 };
-
-const handleEditButtonClick = () => {
+function handleEditButtonClick() {
   toggleOpenPopup();
 };
-
-const handleCloseButtonClick = () => {
+function  handleCloseButtonClick() {
   toggleOpenPopup();
 };
-
-const handleOverlayClick = (event) => {
+function  handleOverlayClick (event) {
   if (event.target === event.currentTarget) {
     toggleOpenPopup();
   }
 };
-
 editButton.addEventListener("click", handleEditButtonClick);
 closeButton.addEventListener("click", handleCloseButtonClick);
-
 popup.addEventListener("click", handleOverlayClick);
 
 
+let formElement = popup.querySelector(".popup__form");
+let nameInput = popup.querySelector(".popup__input_name");
+let descriptionInput = popup.querySelector(".popup__input_description");
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  let nameValue = nameInput.value;
+  let descriptionValue = descriptionInput.value;
+  let profileName = document.querySelector(".profile__name");
+  let profileDescription = document.querySelector(".profile__description");
+  profileName.textContent = nameValue;
+  profileDescription.textContent = descriptionValue;
+  nameInput.value = nameValue;
+  descriptionInput.value = descriptionValue;
+  handleCloseButtonClick();
+}
+formElement.addEventListener('submit', handleFormSubmit);
